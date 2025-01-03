fa5fc57be5eae9cdcaf2d43d8cafe5468679b53e
arthurdlc <delacour.a1109@gmail.com>
2025-01-03 10:27:46 +0100

    dexdc

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a Square class."""
+"""This module defines a function to determine if all boxes can be unlocked."""
 
 
 def canUnlockAll(boxes):
     """
-    Détermine si toutes les boîtes peuvent être ouvertes.
-    :param boxes: Une liste de listes contenant des clés.
-    :return: True si toutes les boîtes peuvent être ouvertes, False sinon.
-    """
-    n = len(boxes)  # Nombre de boîtes
-    opened = [False] * n  # Toutes les boîtes sont fermées au départ
-    opened[0] = True  # La première boîte est toujours ouverte
-    keys = set(boxes[0])  # Clés récupérées dans la première boîte
+    Determines if all boxes can be unlocked.
 
-    while keys:  # Tant qu'il reste des clés à traiter
+    Args:
+        boxes (list of list of int): A list where each sublist represents a box
+                                     and contains keys for other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    n = len(boxes)  # Number of boxes
+    opened = [False] * n  # All boxes are initially closed
+    opened[0] = True  # The first box is always open
+    keys = set(boxes[0])  # Keys collected from the first box
+
+    while keys:  # As long as there are keys to process
         new_keys = set()
         for key in keys:
-            if 0 <= key < n and not opened[key]:  # Clé valide et boîte fermée
-                opened[key] = True  # On ouvre la boîte
-                new_keys.update(boxes[key])  # On récupère les clés de la boîte ouverte
-        keys = new_keys  # Mise à jour des clés à traiter
+            if 0 <= key < n and not opened[key]:  # Valid key and unopened box
+                opened[key] = True  # Open the box
+                new_keys.update(boxes[key])  # Collect keys from the opened box
+        keys = new_keys  # Update keys to process
 
-    return all(opened)  # Vérifie si toutes les boîtes sont ouvertes
+    return all(opened)  # Check if all boxes are opened
