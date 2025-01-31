2e066f94a66be186e7ce66261ffe6e61cb0e5aa1
arthurdlc <delacour.a1109@gmail.com>
2025-01-31 10:52:45 +0100

    cd

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include "sandpiles.h"
+
+/**
+ * print_grid_sandpile - Prints a 3x3 grid
+ * @grid: The grid to print
+ */
+void print_grid_sandpile(int grid[3][3])
+{
+	int i, j;
+
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+/**
+ * is_stable - Checks if a sandpile is stable
+ * @grid: The grid to check
+ * Return: 1 if stable
+ */
+int is_stable(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+				return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * toppling_round - Topples an unstable sandpile
+ * @grid: The 3x3 grid to topple
+ */
+void toppling_round(int grid[3][3])
+{
+	int temp[3][3] = {0};
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				temp[i][j] -= 4;
+				if (i > 0)
+					temp[i - 1][j] += 1;
+				if (i < 2)
+					temp[i + 1][j] += 1;
+				if (j > 0)
+					temp[i][j - 1] += 1;
+				if (j < 2)
+					temp[i][j + 1] += 1;
+			}
+		}
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid[i][j] += temp[i][j];
+		}
+	}
+}
+
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: First stable 3x3 sandpile, result is stored here
+ * @grid2: Second stable 3x3 sandpile
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
+
+	while (!is_stable(grid1))
+	{
+		print_grid_sandpile(grid1);
+		toppling_round(grid1);
+	}
+}
---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef I_DONT_LIKE_SAND
+#define I_DONT_LIKE_SAND
+
+void print_grid_sandpile(int grid[3][3]);
+int is_stable(int grid[3][3]);
+void toppling_round(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif
