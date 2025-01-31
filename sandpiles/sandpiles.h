6442ca85448d0f6a68cf64722d2aad90d62780af
arthurdlc <delacour.a1109@gmail.com>
2025-01-31 11:01:03 +0100

    cd

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,9 +1,15 @@
-#ifndef I_DONT_LIKE_SAND
-#define I_DONT_LIKE_SAND
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+/**
+ * File: sandpiles.h
+ * Description: Header file containing function prototypes
+ * for sandpiles operations.
+ */
 
 void print_grid_sandpile(int grid[3][3]);
 int is_stable(int grid[3][3]);
 void toppling_round(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
-#endif
+#endif /* SANDPILES_H */
