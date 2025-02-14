36b1b3d053b12645703949c7c831309561ee8936
arthurdlc <delacour.a1109@gmail.com>
2025-02-14 13:52:31 +0100

    cd

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,20 +1,52 @@
 #!/usr/bin/python3
+"""
+    Continuously reads input lines and processes them to compute metrics:
+"""
 import sys
-import signal
 
-"""
-This script reads log entries from stdin, extracts status codes and file sizes,
-and prints statistics every 10 lines or upon receiving a keyboard interrupt (CTRL + C).
-"""
-""" DECLARATION DES VARIABLES """"
-int ttl_files = 0
-cEtat = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
+total_file_size = 0
+line_count = 0
+valid_status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+status_code_count = {code: 0 for code in valid_status_codes}
+has_printed_final = False
+
+
+def print_metrics():
+    """
+    Prints the computed metrics to stdout.
+    """
+    print(f"File size: {total_file_size}")
+    for status_code in sorted(valid_status_codes):
+        if status_code_count[status_code] > 0:
+            print(f"{status_code}: {status_code_count[status_code]}")
+
+
+if __name__ == "__main__":
+    try:
+        has_printed_final = False
+
+        for line in sys.stdin:
+            words = line.split()
+
+            if len(words) >= 2:
+                status_code = words[-2]
+                try:
+                    file_size = int(words[-1])
+                    total_file_size += file_size
+                except ValueError:
+                    continue
+                if status_code in status_code_count:
+                    status_code_count[status_code] += 1
+
+                line_count += 1
+
+                if line_count % 10 == 0:
+                    print_metrics()
+                    has_printed_final = True
+
+        if not has_printed_final or line_count % 10 != 0:
+            print_metrics()
+
+    except KeyboardInterrupt:
+        print_metrics()
+        raise
