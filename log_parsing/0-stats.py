d675347a9d2481e0ad90e3f091e35dfeed931b4b
arthurdlc <delacour.a1109@gmail.com>
2025-02-14 14:23:57 +0100

    cd

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -15,7 +15,7 @@ def print_metrics():
     """
     Prints the computed metrics to stdout.
     """
-    print(f"File size: {total_file_size}") # ici on affiche la taille des fichier qui on ete incrementé apres dans le code ci dessous
+    print(f"File size: {total_file_size}") 
     for status_code in sorted(valid_status_codes): # on parcours les codes d'etats
         if status_code_count[status_code] > 0: # si le code d'etat n'est pas egale a 0
             print(f"{status_code}: {status_code_count[status_code]}") # on le print
