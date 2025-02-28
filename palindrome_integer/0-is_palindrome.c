698648289b9b5dfb9db7c47f42badcca74cd4afb
arthurdlc <delacour.a1109@gmail.com>
2025-02-28 14:11:31 +0100

    cd

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "palindrome.h"
+
+int is_palindrome(unsigned long n)
+{
+	unsigned long original = n;
+	unsigned long reversed = 0;
+
+	while (n != 0)
+	{
+		reversed = reversed * 10 + (n % 10);
+		n /= 10;
+	}
+
+	return (reversed == original) ? 1 : 0;
+}
