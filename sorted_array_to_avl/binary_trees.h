9a47a9af3ae5dfd7eccc8df9142f83048e8ff214
arthurdlc <delacour.a1109@gmail.com>
2025-04-06 22:58:02 +0200

    ce checker rend fou pour betty

---
+++ sorted_array_to_avl/binary_trees.h
@@ -0,0 +1,29 @@
+#ifndef BINARY_SHMINARY
+#define BINARY_SHMINARY
+
+#include <stdio.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+
+avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *build_avl(int *array, int start, int end, avl_t *parent);
+avl_t *create_node(avl_t *parent, int value);
+
+#endif
