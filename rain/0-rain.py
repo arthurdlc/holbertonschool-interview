9115140a133846feb6232fa3de51d8a028b2fd7c
arthurdlc <delacour.a1109@gmail.com>
2025-04-25 15:02:43 +0200

    ok

---
+++ rain/0-rain.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+rain retourn la somme de la pluie qui a été retenue
+""""
+def rain(walls)
+    if(wall{0} )
+    for i in range walls[i]
+        assert i => 0
+        
+    
