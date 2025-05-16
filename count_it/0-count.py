c60c1da55fd5ed5064a689f2d8c2831faef0b4e0
arthurdlc <delacour.a1109@gmail.com>
2025-05-16 16:42:06 +0200

    correction pycodestyle

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -8,7 +8,8 @@ def count_words(subreddit, word_list, word_count={}, after=None):
     if not word_list:
         return
 
-    # Normalize word_list to lowercase and remove duplicates while counting duplicates
+    # Normalize word_list to lowercase and remove 
+    # duplicates while counting duplicates
     if not word_count:
         for word in word_list:
             lower = word.lower()
