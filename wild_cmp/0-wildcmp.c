8c6c4cf6b54cc7328a3ee2cba597637d84dfb0f7
arthurdlc <delacour.a1109@gmail.com>
2025-08-01 16:05:45 +0200

    correction betty

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -10,16 +10,16 @@
 int wildcmp(char *s1, char *s2)
 {
 if (*s1 == '\0' && *s2 == '\0')
-return 1;
+return (1);
 
 if (*s2 == '*')
 {
-// Try skipping the '*', or using it to match one or more characters
-return wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2));
+/* Try skipping the '*', or using it to match one or more characters*/
+return (wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2)));
 }
 
 if (*s1 == *s2)
-return wildcmp(s1 + 1, s2 + 1);
+return (wildcmp(s1 + 1, s2 + 1));
 
-return 0;
+return (0);
 }
