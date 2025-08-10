ffbd404e11f5dc0db8211a45f681728b53947f87
arthurdlc <delacour.a1109@gmail.com>
2025-08-10 22:48:41 +0200

    demo

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" pascal triangle module
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows of Pascal's Triangle to generate.
+
+    Returns:
+        list of lists of int: Pascal's Triangle represented as a list of rows.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        row = [1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
